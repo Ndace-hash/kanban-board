633842e8d7780922498d12074574dbc84c2d7a6c
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-06-25 21:44:21 +0100

    removed taskCard

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -8,8 +8,7 @@ import {
 } from "react";
 import "./Board.modules.css";
 
-import DeleteIcon from "@/components/Icons/MaterialSymbolsDeleteOutline";
-import ArrowIcon from "@/components/Icons/BxChevronDown";
+import TaskCard from "./TaskCard";
 import AddIcon from "@/components/Icons/MaterialSymbolsAddRounded";
 interface props {
 	setViewAddModal: Dispatch<SetStateAction<boolean>>;
@@ -24,45 +23,6 @@ interface props {
 		>
 	>;
 }
-interface cardProp {
-	task: {
-		title: string;
-		description: string;
-		id: string;
-	};
-	handleDeleteClick: (id: string) => MouseEventHandler<HTMLButtonElement>;
-}
-const TaskCard = ({ task, handleDeleteClick }: cardProp) => {
-	const [accordionIsOpen, setAccordionIsOpen] = useState(false);
-	return (
-		<article className="border my-3 mx-2 rounded-md bg-green-300 h-max relative">
-			<div className="flex justify-between items-center p-1 relative">
-				<h3 className="font-semibold">{task.title}</h3>
-				<div className="actions flex z-10 items-center absolute right-0 px-3 bg-gradient-to-r from-green-300 to-green-400 h-full rounded-e-sm">
-					<button onClick={() => setAccordionIsOpen(!accordionIsOpen)}>
-						<ArrowIcon
-							className={`duration-200 ${accordionIsOpen ? "rotate-180" : ""}`}
-						/>
-						<span className="sr-only">toggle accordion</span>
-					</button>
-					<button onClick={handleDeleteClick(task.id)}>
-						<DeleteIcon />
-						<span className="sr-only">delete</span>
-					</button>
-				</div>
-			</div>
-			<p
-				className={`duration-700 rounded-sm shadow-inner bg-green-400 py-1 px-2 text-sm w-full ${
-					accordionIsOpen
-						? "translate-y-0"
-						: "absolute -translate-y-3/4 opacity-0"
-				}`}
-			>
-				{task.description}
-			</p>
-		</article>
-	);
-};
 
 export default function Board({ setViewAddModal, tasks, setTasks }: props) {
 	const handleAddClick: MouseEventHandler<HTMLButtonElement> = (e) => {
