74311870cdc62134d50853cc41ecc482edd472a7
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-06-26 22:38:38 +0100

    restructured delete task and made types more concise

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,39 +1,50 @@
 "use client";
-import {
-	Dispatch,
-	Fragment,
-	MouseEventHandler,
-	SetStateAction,
-	useState,
-} from "react";
+import { Dispatch, MouseEventHandler, SetStateAction, useState } from "react";
 import "./Board.modules.css";
 
 import TaskCard from "./TaskCard";
 import AddIcon from "@/components/Icons/MaterialSymbolsAddRounded";
+import { ReactSortable } from "react-sortablejs";
+
+import { Task, SetState, BoardType } from "@/../types";
+
 interface props {
 	setViewAddModal: Dispatch<SetStateAction<boolean>>;
-	tasks: { title: string; description: string; id: string }[] | [];
-	setTasks: Dispatch<
-		SetStateAction<
-			{
-				title: string;
-				description: string;
-				id: string;
-			}[]
-		>
-	>;
+	tasks?: Task[] | [];
+	setTasks: SetState<{ [k: string]: Task[] }>;
+	name: BoardType;
+	setCurrentBoard: SetState<BoardType>;
 }
 
-export default function Board({ setViewAddModal, tasks, setTasks }: props) {
-	const handleAddClick: MouseEventHandler<HTMLButtonElement> = (e) => {
-		e.preventDefault();
-		setViewAddModal(true);
+export default function Board({
+	setViewAddModal,
+	setCurrentBoard,
+	tasks,
+	setTasks,
+	name,
+}: props) {
+	const [tasksList, setTasksList] = useState([]);
+	const handleAddClick: (
+		board: BoardType
+	) => MouseEventHandler<HTMLButtonElement> = (board) => {
+		return (e) => {
+			e.preventDefault();
+			setViewAddModal(true);
+			setCurrentBoard(board);
+		};
 	};
 	const deleteTask = (id: string) => {
 		const action: MouseEventHandler<HTMLButtonElement> = (event) => {
 			event.stopPropagation();
 			setTasks((tasks) => {
-				const newTasks = tasks?.filter((tasks) => tasks.id != id);
+				let newTasksArray: Task[] = [];
+				for (let task of tasks[name]) {
+					if (task.id != id) newTasksArray.push(task);
+				}
+				const newTasks = {
+					...tasks,
+					[name]: [...newTasksArray],
+				};
 				localStorage.setItem("tasks", JSON.stringify(newTasks));
 				return newTasks;
 			});
@@ -44,24 +55,31 @@ export default function Board({ setViewAddModal, tasks, setTasks }: props) {
 	return (
 		<section className="shadow-lg border w-[350px] rounded-md h-80 max-h-96 overflow-auto">
 			<div className="border-b flex justify-between items-center py-2 px-3">
-				<h2 className="">In Progress</h2>
+				<h2 className="">{name.replaceAll("_", " ")}</h2>
 				<button
 					className="text-xs border p-1 rounded-full shadow-md flex justify-center items-center"
-					onClick={handleAddClick}
+					onClick={(e) => {
+						return handleAddClick(name)(e);
+					}}
 				>
 					<AddIcon className="text-xl" />
 					<span className="sr-only">add tasks</span>
 				</button>
 			</div>
 			<div className="py-3">
-				{tasks?.map((task) => (
-					<Fragment key={task.id}>
+				<ReactSortable
+					list={tasksList}
+					setList={setTasksList}
+					group={"taskBoard"}
+				>
+					{tasks?.map((task) => (
 						<TaskCard
 							task={task}
 							handleDeleteClick={() => deleteTask(task.id)}
+							key={task.id}
 						/>
-					</Fragment>
-				))}
+					))}
+				</ReactSortable>
 			</div>
 		</section>
 	);
