2f6b3b713e1c46d257a5aa20d086ef322d151cae
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2024-07-02 23:11:22 +0100

    add colors to classify each task

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -7,19 +7,31 @@ interface CardProps {
 	handleDeleteClick: (id: string) => MouseEventHandler<HTMLButtonElement>;
 	draggable: boolean;
 }
+const priorityGroup = [
+	{ mainColor: "bg-red-400", secondaryColor: "bg-red-500" },
+	{ mainColor: "bg-orange-300", secondaryColor: "bg-orange-400" },
+	{ mainColor: "bg-yellow-300", secondaryColor: "bg-yellow-400" },
+	{ mainColor: "bg-green-300", secondaryColor: "bg-green-400" },
+];
 
 const TaskCard: FC<CardProps> = ({ task, handleDeleteClick, draggable }) => {
 	const [accordionIsOpen, setAccordionIsOpen] = useState(false);
 
 	return (
 		<article
-			className="border my-3 mx-2 rounded-md bg-green-300 h-max relative hover:cursor-grab"
+			className={`border my-3 mx-2 rounded-md h-max relative hover:cursor-grab ${
+				priorityGroup[task.priority!].mainColor
+			}`}
 			draggable={draggable}
 			id={task.id}
 		>
 			<div className="flex justify-between items-center p-1 relative">
 				<h3 className="font-semibold">{task.title}</h3>
-				<div className="actions flex z-10 items-center absolute right-0 px-3 bg-gradient-to-r from-green-300 to-green-400 h-full rounded-e-sm">
+				<div
+					className={`actions flex z-10 items-center absolute right-0 px-3 bg-gradient-to-r from-transparent to-${
+						priorityGroup[task.priority!].mainColor
+					} h-full rounded-e-sm`}
+				>
 					<button onClick={() => setAccordionIsOpen(!accordionIsOpen)}>
 						<ArrowIcon
 							className={`duration-200 ${accordionIsOpen ? "rotate-180" : ""}`}
@@ -33,7 +45,9 @@ const TaskCard: FC<CardProps> = ({ task, handleDeleteClick, draggable }) => {
 				</div>
 			</div>
 			<p
-				className={`duration-700 rounded-sm shadow-inner bg-green-400 py-1 px-2 text-sm w-full ${
+				className={`duration-700 rounded-sm shadow-inner ${
+					priorityGroup[task.priority!].secondaryColor
+				} py-1 px-2 text-sm w-full ${
 					accordionIsOpen
 						? "translate-y-0"
 						: "absolute -translate-y-3/4 opacity-0"
